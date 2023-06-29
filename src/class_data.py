ac65b1d7aff5dc53ca79e86958b4e6ebdee98e7e
Qvert <glscharow@yandex.ru>
2023-06-29 14:18:20 +0300

    add pars_prices

--- src/class_data.py
+++ src/class_data.py
@@ -5,3 +5,7 @@ class ParsingAvito:
     def __init__(self, url, driver):
         self.get_page = driver.get(url)
 
+    def get_prices(self):
+        prices = self.get_page.find_elements(By.CLASS_NAME, 'price-price-JP7qe')
+        list_prices = [i.text for i in prices]
+        return list_prices
